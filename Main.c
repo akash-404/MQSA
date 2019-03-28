5fea067bac50f0348b9dbca881381422fd39ce81
akash-404 <akash.28.2011@gmail.com>
2019-03-28 09:24:04 +0530

    Created main function

--- Main.c
+++ Main.c
@@ -1,6 +1,33 @@
 #include<stdio.h>
+#include<stdlib.h>
+
+int n = 0; 
+
+void input()
+{
+	printf("Enter total number of processes : ");
+	scanf("%d",&n);
+}
 
 void main()
 {
-   printf("Hwllo World");
+	input();
+	int i = 3;
+	while(n>0)
+	{
+		switch(i)
+		{
+			case 1:
+				RoundRobin4();	//top priority
+				break;
+			case 2:
+				PriorityScheduling();	//medium priority
+				break;
+			case 3:
+				FCFS;	//low priority
+				break;
+		}
+		i++;
+		sleep(10);	//time quantum for each of multilevel queue
+	}
 }
