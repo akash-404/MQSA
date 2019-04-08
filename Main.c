17c80e36918b4861898a864dc3833349a7a5817d
akash-404 <akash.28.2011@gmail.com>
2019-04-08 17:39:53 +0530

    functions

--- Main.c
+++ Main.c
@@ -1,24 +1,68 @@
 #include<stdio.h>
 #include<stdlib.h>
 
-int n = 0; 
+int no_of_processes = 0; 
+
+struct p_info
+{
+	int pid;
+	int priority;
+	int burst_time;
+	int waiting_time;
+	int turnaround_time;
+	int remaining_time;
+	int arrival_time;
+};
+
+//List of functions used
+void FCFS();
+void PriorityScheduling();
+void RoundRobin();
+
 
 void input()
 {
 	printf("Enter total number of processes : ");
-	scanf("%d",&n);
+	scanf("%d",&no_of_processes);
+	ptr = (struct p_info*) malloc (no_of_processes * sizeof(struct p_info));
+	int i;
+	bool flag = true;
+	for(i=0; i<no_of_processes; i++)
+	{
+		printf("Enter process id:\t");
+		scanf("%d", &(ptr+i)->pid);
+		printf("Enter details for process\n");
+		while(flag)
+		{
+			printf("Enter priority(from 1 to 10)");
+			scanf("%d", &(ptr+i)->priority);
+			int a = (ptr+i)->priority
+			if(a>=1 && a<=10)
+				flag = false;
+		}
+
+		scanf("%d", &(ptr+i)->pid);
+		printf("Enter burst time");
+		scanf("%d", &(ptr+i)->burst_time);
+		printf("Enter arrival time");
+		scanf("%d", &(ptr+i)->arrival_time);
+		
+
+	}
+
+
 }
 
 void main()
 {
 	input();
 	int i = 3;
-	while(n>0)
+	while(no_of_processes>0)
 	{
 		switch(i)
 		{
 			case 1:
-				RoundRobin4();	//top priority
+				RoundRobin();	//top priority
 				break;
 			case 2:
 				PriorityScheduling();	//medium priority
