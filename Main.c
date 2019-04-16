ffe77883ad07f24086554b8790ccd84bad5f719f
akash-404 <akash.28.2011@gmail.com>
2019-04-16 09:54:21 +0530

    all functions added

--- Main.c
+++ Main.c
@@ -3,9 +3,13 @@
 #include<stdbool.h>
 
 int no_of_processes = 0; 
-int counter=1;
-int q1_no,q2_no,q3_no;
-struct p_info
+int counter=1,timer = 0;
+int n;
+
+
+
+
+typedef struct p_info
 {
 	int pid;
 	int priority;
@@ -13,73 +17,85 @@ struct p_info
 	int waiting_time;
 	int turnaround_time;
 	int remaining_time;
+	int completion_time;
 	int arrival_time;
 	int priority_group;
-	int remaining_time;
-	int flag = 0;
-};
+	int flag;
+}P;
+
+P p_arr[10];
 
 //List of functions used
-void input();
+void input(int);
 void FCFS();
 void PriorityScheduling();
 void RoundRobin();
 void show();
+void check();
 
 
-void check algo()
+
+
+void input(int no_of_processes)
 {
-	if(counter==1)
-		RoundRobin();
-	else if(counter == 2)
-		
-}
-
-
-
-
-void input()
-{
-	printf("Enter total number of processes : ");
-	scanf("%d",&no_of_processes);
+	int i = 0;
 	
-	struct p_info* ptr;
-	ptr = (struct p_info*) malloc (no_of_processes * sizeof(struct p_info));
-	int i;
 	
 	for(i=0; i<no_of_processes; i++)
 	{
 		bool flag = true;
-		printf("Enter process id:\t");
-		scanf("%d", &(ptr+i)->pid);
+		printf("\nEnter process id:\t");
+		scanf("%d", &p_arr[i].pid);
 		printf("Enter details for process\n");
 		while(flag==true)
 		{
 			printf("Enter priority(from 1 to 10): ");
-			scanf("%d", &(ptr+i)->priority);
-			int a = (ptr+i)->priority;
+			scanf("%d", &p_arr[i].priority);
+			int a = p_arr[i].priority;
 			if(a>=1 && a<=10)
 				flag = false;
 		}
 		printf("Enter burst time: ");
-		scanf("%d", &(ptr+i)->burst_time);
+		scanf("%d", &p_arr[i].burst_time);
+		p_arr[i].remaining_time = p_arr[i].burst_time;
 		printf("Enter arrival time: ");
-		scanf("%d", &(ptr+i)->arrival_time);
-		if((ptr+i)->priority<4)
+		scanf("%d", &p_arr[i].arrival_time);
+		
+		
+	///////////// assigning priority group	///////////////	
+		if(p_arr[i].priority<4)
 		{
-			(ptr+i)->priority_group = 1;
+			p_arr[i].priority_group = 1;
 		}
-		else if((ptr+i)->priority>3 && (ptr+i)->priority<7)
+		else if(p_arr[i].priority>3 && p_arr[i].priority<7)
 		{
-			(ptr+i)->priority_group = 2;
+			p_arr[i].priority_group = 2;
 		}
-		else if((ptr+i)->priority>7 && (ptr+i)->priority<11)
+		else if(p_arr[i].priority>7 && p_arr[i].priority<11)
 		{
-			(ptr+i)->priority_group = 3;
+			p_arr[i].priority_group = 3;
 		}
 		
 
 	}
+
+
+
+	///////////////	sorting	according to arrival time//////////
+	int j;
+	for(i=0;i<no_of_processes-1;i++)
+	{
+		for(j=0; j<no_of_processes;j++)
+		{
+			if(p_arr[j].arrival_time > p_arr[i].arrival_time)
+			{
+				struct p_info temp = p_arr[j];
+				p_arr[j] = p_arr[j+1];
+				p_arr[j+1] = temp;
+			}
+		}	
+	}
+	timer = p_arr[0].arrival_time;
 
 
 }
@@ -87,43 +103,222 @@ void input()
 
 void show()
 {
+	printf("Process ID  Priority  Burst Time  Arrival Time  Priority_Group  Remaining Time  flag\n");
 	int i = 0;
-	struct p_info* ptr;
-	for(i=0; i<no_of_processes; i++)
-	{
-		printf("Process ID: %d\t",(ptr+i)->pid);
-		printf("priority : %d\t",(ptr+i)->priority);
-		printf("burst time: %d\t", (ptr+i)->burst_time);
-		printf("arrival time: %d\t",(ptr+i)->arrival_time);
-		printf("Priority Group: %d",(ptr+i)->priority_group);
-		printf("\n");		
-
+	for(i=0; i<n; i++)
+	{		
+		printf("%d \t", p_arr[i].pid);
+		printf("%d ", p_arr[i].completion_time);
 	}
-
-	
 }
+
+
 
 
 void RoundRobin()
 {
-
-	struct p_info* ptr;
-	int i = 0;
-	for(i=0;i<n;i++)
+	int time_quantum = 4;
+	int i = 0, time = 10;
+	for(i=0;i<no_of_processes;i++)
 	{
-		if((ptr+i)->)
+		if(p_arr[i].priority_group == 1 && p_arr[i].arrival_time <= timer)
+		{
+			int rt = p_arr[i].remaining_time;
+			if(rt <= time_quantum && rt>0)
+			{
+				timer+=rt;
+				time += rt;
+				rt = 0;
+				p_arr[i].remaining_time = 0;
+				p_arr[i].flag = 1;
+			}
+			else
+			{
+				rt = rt-time_quantum;
+				p_arr[i].remaining_time -= time_quantum;
+				time+=time_quantum;
+				timer += time_quantum;
+
+			}
+
+			if(rt==0 && p_arr[i].flag == 1)
+			{
+				no_of_processes -= 1;
+				p_arr[i].completion_time = timer;
+			}
+		}
+	}
+	check();
+	FCFS();
+	
+	
+	
+}
+
+
+
+
+
+
+void PriorityScheduling()
+{
+	int i;
+	int time = 10;
+	for(i=0;i<no_of_processes;i++)
+	{
+		if(p_arr[i].priority_group == 2 && p_arr[i].arrival_time <= timer)
+		{
+			int j;
+			for(i=0;i<no_of_processes-1;i++)
+			{
+				for(j=0; j<no_of_processes;j++)
+					{
+						if(p_arr[j].priority > p_arr[i].priority)
+						{
+							struct p_info temp = p_arr[j];
+							p_arr[j] = p_arr[j+1];
+							p_arr[j+1] = temp;
+						}
+					}	
+			}
+			
+			int rt = p_arr[i].remaining_time;
+			if(rt>0 && rt<=10)
+			{
+				timer+=rt;
+				time -= rt;
+				rt = 0;
+				p_arr[i].remaining_time = 0;
+				p_arr[i].flag = 1;
+			}
+			else
+			{
+				timer+=10;
+				time -=10;
+				p_arr[i].remaining_time = p_arr[i].remaining_time-10;
+			}
+
+			if(rt==0 && p_arr[i].flag == 1)
+			{
+				no_of_processes -= 1;
+				p_arr[i].completion_time = timer;
+			}
+		}
+	}
+	
+	
+	
+}
+
+
+
+
+
+
+
+void FCFS()
+{
+	int i;
+	int time = 10;
+	for(i=0;i<no_of_processes;i++)
+	{
+		if(p_arr[i].priority_group == 3 && p_arr[i].arrival_time <= timer)
+		{
+			int rt = p_arr[i].remaining_time;
+			if(rt>0 && rt<=10)
+			{
+				timer+=rt;
+				time -= rt;
+				rt = 0;
+				p_arr[i].remaining_time = 0;
+				p_arr[i].flag = 1;
+			}
+			else
+			{
+				timer+=10;
+				time -=10;
+				p_arr[i].remaining_time = p_arr[i].remaining_time-10;
+			}
+
+			if(rt==0 && p_arr[i].flag == 1)
+			{
+				no_of_processes -= 1;
+				p_arr[i].completion_time = timer;
+			}
+		}
+	}	
+	
+
+	check();
+	PriorityScheduling();
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+void check()
+{
+	int count = 0;
+	for(int i=0;i<n;i++)
+	{
+		if(p_arr[i].remaining_time==0)
+		{
+			count = count+1;
+		}
+	}
+	if(count==n)
+	{
+		show();
+	}
+	else
+	{
+		if(counter == 1)
+			RoundRobin();
+		else if(counter == 2)
+			PriorityScheduling();
+		else if(counter == 3)
+			FCFS();
+
 	}
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
 void main()
 {
-	input();
-	int i = 3;
-	while(no_of_processes>0)
-	{
-		check_algo();
-	}
-	show();
+	printf("Enter total number of processes : ");
+	scanf("%d",&no_of_processes);
+	n = no_of_processes;
+	input(no_of_processes);
+	check();
 }
+
+
